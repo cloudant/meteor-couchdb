9330e7f1664277174bbc94db8550cf30ac31217f
mariobriggs <mariobriggs@in.ibm.com>
2015-10-01 23:48:34 +0530

    bumped version to 0.0.5

--- package.js
+++ package.js
@@ -24,7 +24,7 @@
 
 Package.describe({
   name: 'cloudant:couchdb',
-  version: '0.0.4',
+  version: '0.0.5',
   // Brief, one-line summary of the package.
   summary: 'Full stack database driver for CouchDB/Cloudant in Meteor',
   // URL to the Git repository containing the source code for this package.
